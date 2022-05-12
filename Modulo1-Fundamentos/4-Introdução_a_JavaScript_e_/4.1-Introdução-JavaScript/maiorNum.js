a5f77ab09786547f09b43ce14c4b835fb4923d4a
Danilo Ramalho <daniloti@hotmail.com>
2022-05-12 18:57:03 -0300

    condição numero maior

---
+++ Modulo1-Fundamentos/4-Introdução_a_JavaScript_e_/4.1-Introdução-JavaScript/maiorNum.js
@@ -0,0 +1,11 @@
+let valor1 = 71, valor2 = 1, valor3 = 54;
+
+if(valor1 > valor2 && valor1 > valor3 ){
+   console.log(valor1);
+   
+}else if(valor2 > valor1 && valor2 > valor3){
+  console.log(valor2);
+
+}else{
+    console.log(valor3);
+}
