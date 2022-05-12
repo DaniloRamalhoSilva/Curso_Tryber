6e042b3a7dcfafe11572c78b5b21eb9253dd6ab3
Danilo Ramalho <daniloti@hotmail.com>
2022-05-12 18:45:36 -0300

    operadores

---
+++ Modulo1-Fundamentos/4-Introdução_a_JavaScript_e_/4.1-Introdução-JavaScript/operadores.js
@@ -0,0 +1,13 @@
+const a =5;
+const b = 7;
+let = resultado = 0;
+
+resultado = a + b;
+console.log(resultado);
+resultado = a - b;
+console.log(resultado);
+resultado = a * b;
+console.log(resultado);
+resultado = a / b;
+console.log(resultado);
+resultado = a % b;
