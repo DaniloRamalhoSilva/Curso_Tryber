12bb9e45a6cd82351747e81c98a1c23c437d7027
Danilo Ramalho <daniloti@hotmail.com>
2022-05-13 15:54:49 -0300

    praticando o conteudo

---
+++ Modulo1-Fundamentos/4-Introdução_a_JavaScript_e_Logica_de_Programação/4.2_JavaScript-Primeiros-passos/conteudo4.2.js
@@ -0,0 +1,22 @@
+//Obtenha o valor "Serviços" do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let menuServices = menu[1]; 
+console.log(menuServices);
+
+
+//Procure o índice do valor "Portfólio" do array menu:
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let indexOfPortfolio =  menu1.indexOf('Serviços');
+console.log(indexOfPortfolio);
+
+
+//Adicione o valor "Contato" no final do array menu:
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu2.push('Contato')
+
+console.log(menu2);
