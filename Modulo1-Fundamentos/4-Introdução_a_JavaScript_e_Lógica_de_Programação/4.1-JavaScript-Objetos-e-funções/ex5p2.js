37d82aeda2978d411c86a3fd4e2ff602f6740bf1
Danilo Ramalho <daniloti@hotmail.com>
2022-05-19 15:13:52 -0300

    organizando pastas e arquivos

---
+++ Modulo1-Fundamentos/4-Introdução_a_JavaScript_e_Lógica_de_Programação/4.1-JavaScript-Objetos-e-funções/ex5p2.js
@@ -0,0 +1,17 @@
+//🚀 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+//Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+//Valor esperado no retorno da função: 2.
+
+const teste = [2, 3, 2, 5, 8, 2, 3];
+
+
+
+for (let index = 0; index < teste.length; index++) {
+  
+    for(i=0; i < ){
+
+    }
+
+
+    
+}
