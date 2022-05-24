a79232fe051007154f6710631327c67134f26dcb
Danilo Ramalho <daniloti@hotmail.com>
2022-05-24 20:20:52 -0300

    exercicios 5.3  concluido do 4 ao 10 + bonus

--- Modulo1-Fundamentos/5-JavaScript:_DOM_ Eventos_e_Web_Storage/3-JavaScript-Eventos/exercicios/script.js
+++ Modulo1-Fundamentos/5-JavaScript:_DOM_ Eventos_e_Web_Storage/3-JavaScript-Eventos/exercicios/script.js
@@ -15,19 +15,18 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 
-
-
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 const listaDias = document.getElementById('days');
 const conteinerDoBotao = document.getElementsByClassName('buttons-container');
+const divTarefas = document.getElementsByClassName('my-tasks')
 
 
 function criaTeg(tag) {
   return document.createElement(tag);
 };
 
-
+criaDias(dezDaysList);
 function criaDias(dias) {
   for (let dia of dias) {
     let li = criaTeg('li');
@@ -47,7 +46,6 @@ function criaDias(dias) {
 };
 
 feriados('Feriados');
-
 function feriados(text) {
   let botao = criaTeg('button');
   botao.id = 'btn-holiday';
@@ -56,21 +54,122 @@ function feriados(text) {
   conteinerDoBotao[0].appendChild(botao);
 };
 
-const botao = document.getElementById('btn-holiday');
+const botaoFeriado = document.getElementById('btn-holiday');
 
-botao.addEventListener('click', function () {
+botaoFeriado.addEventListener('click', function () {
   let disDeFeriados = document.getElementsByClassName('holiday');
-  if(disDeFeriados[0].style.backgroundColor === 'yellow'){
-    for(let dia of disDeFeriados){
+  if (disDeFeriados[0].style.backgroundColor === 'yellow') {
+    for (let dia of disDeFeriados) {
       dia.style.backgroundColor = 'rgb(238,238,238)';
     }
-  }else{
-    for(let dia of disDeFeriados){
+  } else {
+    for (let dia of disDeFeriados) {
       dia.style.backgroundColor = 'yellow';
     }
   }
-  
-  
 });
 
-criaDias(dezDaysList);
+sextaFeira('Sexta-feira');
+function sextaFeira(text) {
+  let botao = criaTeg('button');
+  botao.id = 'btn-friday';
+  botao.name = text;
+
+  conteinerDoBotao[0].appendChild(botao);
+};
+
+const botaoSexta = document.getElementById('btn-friday');
+var diasDeSexta = [];
+
+botaoSexta.addEventListener('click', function () {
+  let sextaFeiras = document.getElementsByClassName('friday');
+
+  if (sextaFeiras[0].innerText === 'sextou') {
+    for (let i = 0; i < sextaFeiras.length; i += 1) {
+      sextaFeiras[i].innerText = diasDeSexta[i];
+    }
+
+  } else {
+    diasDeSexta = [];
+    for (let sexta of sextaFeiras) {
+      diasDeSexta.push(sexta.innerText);
+      sexta.innerText = 'sextou';
+    }
+
+  }
+});
+
+let diasDaLista = document.querySelectorAll('.day')
+
+for (let dia of diasDaLista) {
+  dia.addEventListener('mouseover', zommAplica);
+  dia.addEventListener('mouseout', zommDesaplica);
+  dia.addEventListener('click', addTarefa);
+}
+
+function zommAplica(event) {
+  event.target.style.fontSize = '30px';
+};
+
+function zommDesaplica(event) {
+  event.target.style.fontSize = '20px';
+};
+
+adicionaUmaTarefa('cozinhar');
+
+function adicionaUmaTarefa(tarefa) {
+  let tagTarefa = criaTeg('span');
+  tagTarefa.innerText = tarefa;
+  divTarefas[0].appendChild(tagTarefa);
+}
+
+adicionaLegenda('red');
+function adicionaLegenda(cor) {
+  let task = criaTeg('div');
+  task.className = 'task';
+  task.style.backgroundColor = cor;
+  divTarefas[0].appendChild(task);
+}
+
+
+let tarefas = document.querySelectorAll('.task')
+
+for (let tarefa of tarefas) {
+  tarefa.addEventListener('click', selecionarTarefa);
+
+}
+
+function selecionarTarefa(event) {
+  if (event.target.className === 'task selected') {
+    event.target.className = 'task';
+
+  } else {
+    event.target.className = 'task selected';
+  }
+};
+
+function addTarefa(event) {
+  event.target.style.backgroundColor = document.querySelectorAll('.selected')[0].style.backgroundColor;
+}
+
+let listCompromisso = document.getElementsByClassName('input-container');
+
+let compromissoLi = listCompromisso[0].appendChild(criaTeg('ul'));
+
+function copromisso() {
+  if (textBox.value === '' ) {
+    alert('Insira uma tarefa valida!');
+  }else{
+    let tagCompromisso = criaTeg('li');
+    tagCompromisso.innerText = textBox.value;
+    tagCompromisso.style.paddingTop = '10px' ;
+    compromissoLi.appendChild(tagCompromisso);
+    textBox.value = '';
+  }
+  
+}
+
+let botao = document.getElementById('btn-add');
+let textBox = document.getElementById('task-input');
+
+botao.addEventListener('click', copromisso)
