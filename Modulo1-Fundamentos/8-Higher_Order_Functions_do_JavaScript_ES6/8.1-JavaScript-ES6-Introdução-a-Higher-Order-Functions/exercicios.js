88c3fba3801814ac3f8394dc950d2f6786be3d68
Danilo Ramalho <daniloti@hotmail.com>
2022-06-24 10:27:35 -0300

    add exercicio 2 e 3

--- Modulo1-Fundamentos/8-Higher_Order_Functions_do_JavaScript_ES6/8.1-JavaScript-ES6-Introdução-a-Higher-Order-Functions/exercicios.js
+++ Modulo1-Fundamentos/8-Higher_Order_Functions_do_JavaScript_ES6/8.1-JavaScript-ES6-Introdução-a-Higher-Order-Functions/exercicios.js
@@ -18,3 +18,41 @@ const criaEmail = (nome) => {
 }
 
 console.log(newEmployees(criaEmail));
+
+
+//2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const checaAposta = (sortesdo, numeroApostado) => sortesdo === numeroApostado;
+
+const resultadoDoSorteio = (numeroApostado, callBack) => 
+callBack(Math.floor(Math.random() * 6), numeroApostado)? 'Parabéns você ganhou' : 'Tente novamente';
+
+console.log(resultadoDoSorteio(4, checaAposta));
+
+
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS =   ['A', 'C',   'B', 'D', 'A', 'A', 'D',   'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const contaPonto = (element, resposta) => {  
+  if(element === resposta){
+    return 1;
+  }
+  if(resposta === 'N.A'){
+    return 0;
+  }
+  return -0.5 
+} 
+
+const verificaResposta = (gabarito, respostas, callBack) => {
+  let pontos = 0;
+  gabarito.forEach((element, index) => {
+    pontos += callBack(element, respostas[index]);    
+  });
+  return pontos;
+}
+
+console.log(verificaResposta(RIGHT_ANSWERS, STUDENT_ANSWERS, contaPonto));
