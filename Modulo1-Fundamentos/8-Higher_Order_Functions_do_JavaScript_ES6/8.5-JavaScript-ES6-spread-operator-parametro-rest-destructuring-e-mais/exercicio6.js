d8adc635ea48b69b3ab592ed35edc4f092932757
Danilo Ramalho <daniloti@hotmail.com>
2022-06-29 11:58:35 -0300

    exercicio 6

--- Modulo1-Fundamentos/8-Higher_Order_Functions_do_JavaScript_ES6/8.5-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
+++ Modulo1-Fundamentos/8-Higher_Order_Functions_do_JavaScript_ES6/8.5-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
@@ -5,4 +5,10 @@ const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
+// exemplo    =>     const [nome, marca, ano] = palio
+
 // escreva toObject abaixo
+
+const toObject = ([nome, marca, ano]) => ({ nome, marca, ano});
+
+console.log(toObject(palio));
