0fa551d4ca0dc502a449212a4e1114dd4200836a
Danilo Ramalho <daniloti@hotmail.com>
2022-07-19 12:28:58 -0300

    Para Fixar - Lista de componentes e chaves

--- Modulo2-Desenvolvimento-Front-end/10-Introdução_a_React/10.2-Componentes-React/fixation-exercises-10-2/src/App.js
+++ Modulo2-Desenvolvimento-Front-end/10-Introdução_a_React/10.2-Componentes-React/fixation-exercises-10-2/src/App.js
@@ -5,31 +5,31 @@ import Order from './order';
 
 function App() {
 
-  const headphone = {
-    id: 102,
-    user: "cena@gmail.com",
-    product: "Razer Headphone",
-    price: {
-      value: 99.99,
-      currency: "dollars"
-    }
-  };
+  const users = [
+    {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    },
 
-  const energyDrink = {
-    id: 77,
-    user: "cena@gmail.com",
-    product: "Monster 500mL",
-    price: {
-      value: 9.99,
-      currency: "dollars"
+    {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
     }
-  };
-
+  ];
   return (
     <div className="App">
       <h1> Orders recently created </h1>
-      <Order order={headphone}/>
-      <Order order={energyDrink}/>
+      {users.map((user, index) => <Order key={index} order={user} />)}      
       <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
     </div>
 
